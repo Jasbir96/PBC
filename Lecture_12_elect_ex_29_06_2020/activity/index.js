2756e1a667d279ec4ce9587e8e36cd4d86c193bd
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-01 21:39:37 +0530

    added set formula code and feature comments

--- Lecture_12_elect_ex_29_06_2020/activity/index.js
+++ Lecture_12_elect_ex_29_06_2020/activity/index.js
@@ -3,8 +3,12 @@ const $ = require('jquery');
 $(document).ready(function(){
 
     let data = [];
+    let lsc;
 
     $('#grid .cell').on('click', function(){
+
+        //catch last clicked cell - done
+        lsc = this;
 
         let rid = parseInt($(this).attr('row-id'));
         let cid = parseInt($(this).attr('col-id'));
@@ -12,15 +16,98 @@ $(document).ready(function(){
         let cidAddr = String.fromCharCode(cid + 65);
         $('#text-input').val(cidAddr + (rid+1));
 
+        //update formula bar value
+
     })
 
     $('#grid .cell').on('blur', function(){
-        console.log("blurred");
+        
+        //if value is not changed to do nothing
+        //return without doing anything
+
+        //delete formula
+
+        //update cell and dependent cells - downstream cells
     })
 
-    // $('#formula-input').on('blur', function(){
+    $('#formula-input').on('blur', function(){
 
-    // })
+        // find a way to work on last clicked cell - done
+
+        // delete the exisiting formula from that cell
+
+        //set the new formula for that cell
+
+        //evalute value from the formula
+
+        //update cell and dependents
+
+    })
+
+    function deleteFormula(cellObject) {
+
+    }
+
+    function evaluate(cell){
+
+    }
+
+    function setFormula(cellObject, formula){
+
+        formula = formula.replace('(', '').replace(')','');
+
+        let formulaComponents = formula.split(' ');
+
+        for(let i=0;i<formulaComponents.length;i++){
+
+            // work for only addreses
+            let chCode = formulaComponents[i].charCodeAt(0);
+
+            // let cell = getCell(cellObject);
+            
+            if(chCode >= 65 && chCode <= 90){
+
+                let upstreamAddress = getIndicesFromAddress(formulaComponents[i]);
+                let myIndices = getIndices(cellObject);
+                //set my upstream to cells i am dependent on
+                data[myIndices.rid][myIndices.cid].upstream.push({
+                    rid:upstreamAddress.rid,
+                    cid:upstreamAddress.cid
+                })
+
+                //set their downstream
+                // as in jispr mai depend krta hu uske downsteam me merko daal do
+                data[upstreamAddress.rid][upstreamAddress.cid].downstream.push({
+                    rid:myIndices.rid,
+                    cid:myIndices.cid
+                })
+            }
+        }
+    }
+
+    function getIndices(cellObject){
+        let rid = parseInt($(this).attr('row-id'));
+        let cid = parseInt($(this).attr('col-id'));
+
+        return {
+            rid:rid,
+            cid:cid
+        }
+    }
+
+    function getIndicesFromAddress(cellAddress){
+        let rid = parseInt(cellAddress.substr(1));
+        let cid = cellAddress.charCodeAt(0) - 65;
+
+        return {
+            rid : rid -1,
+            cid:cid
+        }
+    }
+
+    function updateCell(cellObject){
+
+    }
 
     function init(){
 
@@ -29,9 +116,14 @@ $(document).ready(function(){
 
             let row = [];
             $(this).find('.cell').each(function(){
-                let cell = '';
+                let cell = {
+                    value : '',
+                    formula : '',
+                    upstream : [],
+                    downstream : [] 
+                };
 
-                $(this).html('');
+                $(this).html(cell.value);
                 row.push(cell);
 
             })
